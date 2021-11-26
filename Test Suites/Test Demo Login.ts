058704f3d2bf3892fa9b030573397847f69a5946
Vinh Nguyen <4501104279@student.hcmue.edu.vn>
2021-11-26 21:08:03 +0700

    upload

---
+++ Test Suites/Test Demo Login.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Demo Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5dcc8244-52ef-40ac-829e-6eb72fa40159</testSuiteGuid>
+   <testCaseLink>
+      <guid>162e57d3-793e-4a9d-ba5e-294e616183b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c1ca9454-00f6-4527-b114-f2907bea63a8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9070d4d9-8ce8-40f5-a45f-8cf71391e9c7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9b8c0ed2-8e97-4f24-b08d-90b951dae3a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ae2fa45-c673-4788-a534-71392279e256</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
